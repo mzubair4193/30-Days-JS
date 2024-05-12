de2489df5bbb488ba90d07ba2a2fcb889ed8ac25
Muhammad Zubair <mzubair4193@gmail.com>
2024-05-12 19:24:12 -0400

    day 3 full solve

--- Day_3.js
+++ Day_3.js
@@ -73,3 +73,19 @@ let DdMmYYy = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()} ${date
 console.log(YYYMMDD);
 console.log(DDMMYYYY);
 console.log(DdMmYYy);
+
+const firstName = "victor"
+const lastName = "kenneth"
+const country = "Nigeria"
+const city = "lagos"
+const age = 19;
+const isMarried = false;
+const year = new Date().getFullYear();
+
+let vars = [firstName, lastName, country, city, age, isMarried, year];
+vars.forEach(function (element) {
+    console.log(typeof element);
+});
+
+console.log("10" === 10)
+console.log(parseInt(9.8) === 10);
