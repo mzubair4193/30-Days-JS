a490b6d1a31949f2494c0df7d90cd01ec8b97b7c
Muhammad Zubair <mzubair4193@gmail.com>
2024-05-12 19:21:41 -0400

    day 2 partial solution

--- Day_2.js
+++ Day_2.js
@@ -1,1 +1,11 @@
 /*Day 2 of 30 days JS */
+let operandOne = 4;
+let operandTwo = 3;
+console.log(operandOne + operandTwo);
+console.log(operandOne - operandTwo);
+console.log(operandOne * operandTwo);
+console.log(operandOne / operandTwo);
+console.log(operandOne % operandTwo);
+console.log(operandOne ** operandTwo);
+console.log(operandOne++);
+console.log(operandTwo--);
