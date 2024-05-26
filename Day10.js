669664278d2d8f6d04f6f0d3edc6bb1077eb6d08
Muhammad Zubair <mzubair4193@gmail.com>
2024-05-26 01:52:57 -0400

    day 10 75% complete

--- Day10.js
+++ Day10.js
@@ -23,3 +23,17 @@ for (let i = 0; i < countries.length; i++){
         countriesLength.add(countries[i].length);
 }
 console.log(countriesLength);
+
+
+let a = [ 4, 5, 8, 9]
+let b = [3, 4, 5, 7]
+let A = new Set(a);
+let B = new Set(b);
+
+let intersection = a.filter(num => {
+    return B.has(num)
+})
+console.log(intersection)
+
+let union = [...a, ...b]
+console.log(union);
