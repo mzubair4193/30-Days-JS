cfffd3095f74b431f791165a2a4b2c16235fdd30
Muhammad Zubair <mzubair4193@gmail.com>
2024-05-30 23:43:04 -0400

    day 11 majority complete

--- Day11.js
+++ Day11.js
@@ -53,3 +53,67 @@ const student = ['David', ['HTM', 'CSS', 'JS', 'React'],
 ]
 // let [name, skills, scores, javaScriptScores, reactScores] = student;
 // console.log(name, skills, scores, javaScriptScores, reactScores)
+
+const users = [{
+    name: 'Brook',
+    scores: 75,
+    skills: ['HTM', 'CSS', 'JS'],
+    age: 16
+},
+{
+    name: 'Alex',
+    scores: 80,
+    skills: ['HTM', 'CSS', 'JS'],
+    age: 18
+},
+{
+    name: 'David',
+    scores: 75,
+    skills: ['HTM', 'CSS'],
+    age: 22
+},
+{
+    name: 'John',
+    scores: 85,
+    skills: ['HTML'],
+    age: 25
+},
+{
+    name: 'Sara',
+    scores: 95,
+    skills: ['HTM', 'CSS', 'JS'],
+    age: 26
+},
+{
+    name: 'Martha',
+    scores: 80,
+    skills: ['HTM', 'CSS', 'JS'],
+    age: 18
+},
+{
+    name: 'Thomas',
+    scores: 90,
+    skills: ['HTM', 'CSS', 'JS'],
+    age: 20
+}
+]
+
+for ({
+    name,
+    scores,
+    skills,
+    age
+} of users) {
+console.log(name, scores, skills, age)
+}
+
+for ({
+    name,
+    scores,
+    skills,
+    age
+} of users) {
+if (skills.length < 2) {
+    console.log(name, scores, skills, age)
+}
+}
