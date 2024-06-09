6b4bde69fc688c3fb111c029f85b8c5ed2c24602
Muhammad Zubair <mzubair4193@gmail.com>
2024-06-08 22:30:16 -0400

    day 18 solved/finished

--- Day18.js
+++ Day18.js
@@ -44,3 +44,22 @@ fetchData();
 //     console.log(`country:${country.name} capital:${country.capital} languages:${country.languages} population${population}`)
 // })
 
+//cat api using promise
+let catNames = [];
+let cats = []
+const catsAPI = 'https://api.thecatapi.com/v1/breeds'
+fetch(catsAPI)
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(cat => {
+            cat.push(cat);
+        })
+    })
+    .catch(error => console.log(error))
+
+if (catNames) {
+    cats.forEach(cat => {
+        catNames.push(cat.name)
+    })
+    console.log(catNames)
+}
